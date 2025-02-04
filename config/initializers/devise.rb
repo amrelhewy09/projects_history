a8993a1ba120e8994cab5b9690ee7b6cc578e11b
amr elhewi <amr.elhewi@espace.com.eg>
2025-02-04 13:46:05 +0200

    Add client id and secret to env

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -272,7 +272,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :google_oauth2, '1086175747832-doeid7imcenprps7tdspep6f72kb5hjh.apps.googleusercontent.com','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'
+  config.omniauth :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET']
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
